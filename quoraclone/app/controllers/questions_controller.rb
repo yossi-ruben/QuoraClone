67149da1099aa6b1e98fde318e05aca6c5430432
Yossi Ruben <yossimruben@gmail.com>
2016-11-10 13:36:03 -0500

    removed q show bug

--- quoraclone/app/controllers/questions_controller.rb
+++ quoraclone/app/controllers/questions_controller.rb
@@ -14,7 +14,7 @@ class QuestionsController < ApplicationController
 
   def show
     # binding.pry
-    @question = Question.find_by(params[:id])
+    @question = Question.find_by(id: params[:id])
   end
 
   def questions_query
